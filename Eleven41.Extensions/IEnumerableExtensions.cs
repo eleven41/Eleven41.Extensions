c1e2e182137ad28943b0bff160c105e46469cb2f
Matt Houser <matt@eleven41.com>
2019-07-23 10:55:34 -0400

    Add SafeSequenceEqual method

--- Eleven41.Extensions/IEnumerableExtensions.cs
+++ Eleven41.Extensions/IEnumerableExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Eleven41.Extensions
 {
@@ -13,5 +12,23 @@ namespace Eleven41.Extensions
 				return new T[0]; // Empty array is IEnumerable
 			return self;
 		}
-	}
+
+        /// <summary>
+        /// Determines whether two sequences are equal by comparing the elements by using the default equality comparer for their type.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the input sequences.</typeparam>
+        /// <param name="self">An IEnumerable<T> to compare to other. Can be null.</param>
+        /// <param name="other">An IEnumerable<T> to compare to the first sequence. Can be null.</param>
+        /// <returns>true if the two source sequences are both null or of equal length and their corresponding elements are equal according to the default equality comparer for their type; otherwise, false.</returns>
+        public static bool SafeSequenceEqual<T>(this IEnumerable<T> self, IEnumerable<T> other)
+        {
+            if (self == null && other == null)
+                return true;
+            if (self == null)
+                return false;
+            if (other == null)
+                return false;
+            return self.SequenceEqual(other);
+        }
+    }
 }
---
+++ UnitTests/IEnumerableTests/SafeSequenceEqual_Tests.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Eleven41.Extensions;
+
+namespace UnitTests.IEnumerableTests
+{
+    [TestClass]
+    public class SafeSequenceEqual_Tests
+    {
+        [TestMethod]
+        public void null_to_null()
+        {
+            // Setup
+            IEnumerable<int> source = null;
+            IEnumerable<int> other = null;
+
+            // Execute
+            var result = source.SafeSequenceEqual(other);
+
+            // Check result
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void null_to_empty()
+        {
+            // Setup
+            IEnumerable<int> source = null;
+            IEnumerable<int> other = new List<int>();
+
+            // Execute
+            var result = source.SafeSequenceEqual(other);
+
+            // Check result
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void empty_to_null()
+        {
+            // Setup
+            IEnumerable<int> source = new List<int>();
+            IEnumerable<int> other = null;
+
+            // Execute
+            var result = source.SafeSequenceEqual(other);
+
+            // Check result
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void empty_to_empty()
+        {
+            // Setup
+            IEnumerable<int> source = new List<int>();
+            IEnumerable<int> other = new List<int>();
+
+            // Execute
+            var result = source.SafeSequenceEqual(other);
+
+            // Check result
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void empty_to_non_empty()
+        {
+            // Setup
+            IEnumerable<int> source = new List<int>();
+            IEnumerable<int> other = new List<int>() { 1 };
+
+            // Execute
+            var result = source.SafeSequenceEqual(other);
+
+            // Check result
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void non_empty_to_non_empty_same()
+        {
+            // Setup
+            IEnumerable<int> source = new List<int>() { 1 };
+            IEnumerable<int> other = new List<int>() { 1 };
+
+            // Execute
+            var result = source.SafeSequenceEqual(other);
+
+            // Check result
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void non_empty_to_non_empty_different_values()
+        {
+            // Setup
+            IEnumerable<int> source = new List<int>() { 1 };
+            IEnumerable<int> other = new List<int>() { 2 };
+
+            // Execute
+            var result = source.SafeSequenceEqual(other);
+
+            // Check result
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void non_empty_to_non_empty_different_lengths()
+        {
+            // Setup
+            IEnumerable<int> source = new List<int>() { 1 };
+            IEnumerable<int> other = new List<int>() { 1, 1 };
+
+            // Execute
+            var result = source.SafeSequenceEqual(other);
+
+            // Check result
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void non_empty_to_non_empty_different_order()
+        {
+            // Setup
+            IEnumerable<int> source = new List<int>() { 1, 2 };
+            IEnumerable<int> other = new List<int>() { 2, 1 };
+
+            // Execute
+            var result = source.SafeSequenceEqual(other);
+
+            // Check result
+            Assert.IsFalse(result);
+        }
+    }
+}
